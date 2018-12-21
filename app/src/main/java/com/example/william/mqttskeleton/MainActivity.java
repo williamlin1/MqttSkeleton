b2f7f95379c4b127efafa41637b6bb5c8d88c450
William Lin <williamlinsfms@gmail.com>
2018-12-21 13:08:39 -0800

    Added in working sliderbar

--- app/src/main/java/com/example/william/mqttskeleton/MainActivity.java
+++ app/src/main/java/com/example/william/mqttskeleton/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.SeekBar;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
@@ -14,60 +13,23 @@ public class MainActivity extends AppCompatActivity {
 
     MqttHelper mqttHelper;
 
-    SeekBar bar;
-
-    boolean on = false;
-
-    String topic;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
-        bar = findViewById(R.id.seekBar);
-        bar.setMax(100);
-        bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress,
-                                          boolean fromUser) {
-                if(progress <=55 && progress >= 45){
-                    byte[] send = {0};
-                    mqttHelper.publish(send, "Stop");
-                }else if(progress <=45){
-                    byte[] send = {0};
-                    mqttHelper.publish(send, "Left");
-                }else if(progress >=55){
-                    byte[] send = {0};
-                    mqttHelper.publish(send, "Right");
-                }
-                seekBar.setMax(100);
-            }
-        });
         startMqtt();
-
-
-
     }
 
-    public void Off(View view){
-        on = false;
+    public void left(View view){
+        byte[] send = {1,2,3};
+        mqttHelper.publish(send, "Left");
     }
 
-    public void On(View view){
-        on = true;
+    public void right(View view){
+        byte[] send = {4,5,6};
+        mqttHelper.publish(send,"Right");
     }
 
 
